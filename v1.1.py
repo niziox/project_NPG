2fadd55ffb712c085a349488fd62f21ec7332b7a
Arkadiusz Kontek <arek8131@op.pl>
2020-09-01 15:29:24 +0200

    Dodanie funkcji opcji zapisu oraz dodania nowej pozycji do bazy danych

--- v1.1.py
+++ v1.1.py
@@ -42,7 +42,53 @@ def help_info():
 
 
 def add():
-    pass
+    # Create a database or connect to one
+    file = open('paths/path_file.txt', 'a+')
+    file.seek(0)
+    path = file.readline()
+    data = sqlite3.connect(f'{path}.db')
+    file.close()
+
+    # Create cursor
+    cursor = data.cursor()
+
+    # Create table
+    systolic_arterial_pressure = int(measure1_entry.get())
+    diastolic_blood_pressure = int(measure2_entry.get())
+    current_date = datetime.now()
+    formatted_date = current_date.strftime('%d.%m.%Y')
+
+    if 0 <= systolic_arterial_pressure < 120 and 0 <= diastolic_blood_pressure < 80:
+        messagebox.showinfo(title='Powiadominenie o normie', message='Twoje ciśnienie jest optymalne')
+    elif 0 <= systolic_arterial_pressure <= 129 and 0 <= diastolic_blood_pressure <= 84:
+        messagebox.showinfo(title='Powiadominenie o normie', message='Twoje ciśnienie jest prawidłowe')
+    elif 0 <= systolic_arterial_pressure <= 139 and 0 <= diastolic_blood_pressure <= 89:
+        messagebox.showinfo(title='Powiadominenie o normie', message='Twoje ciśnienie jest prawidłowe wysokie')
+    elif 0 <= systolic_arterial_pressure <= 159 and 0 <= diastolic_blood_pressure <= 99:
+        messagebox.showwarning(title='Powiadominenie o normie', message='Masz łagodne nadciśnienie')
+    elif 0 <= systolic_arterial_pressure <= 179 and 0 <= diastolic_blood_pressure <= 109:
+        messagebox.showwarning(title='Powiadominenie o normie', message='Masz umiarkowane nadciśnienie')
+    elif 0 <= systolic_arterial_pressure <= 1000 and 0 <= diastolic_blood_pressure <= 1000:
+        messagebox.showwarning(title='Powiadominenie o normie', message='Masz ciężkie nadciśnienie')
+    else:
+        messagebox.showerror(title='Powiadominenie o normie', message='Ciężko określić')
+
+    cursor.execute(
+        "INSERT INTO addresses VALUES (:systolic_arterial_pressure, :diastolic_blood_pressure, :formatted_date)",
+        {
+            'systolic_arterial_pressure': systolic_arterial_pressure,
+            'diastolic_blood_pressure': diastolic_blood_pressure,
+            'formatted_date': formatted_date
+        })
+
+    # Commit Changes
+    data.commit()
+
+    # Close Connection
+    data.close()
+
+    measure1_entry.delete(0, END)
+    measure2_entry.delete(0, END)
 
 
 def add_screen():
@@ -75,8 +121,78 @@ def add_screen():
     back_from_adding_button.grid(row=5, column=0, pady=5, columnspan=2)
 
 
+def new_path_window(status):
+    file = open('paths/path_file.txt', 'a+')
+    file.seek(0)
+    path = file.readline()
+    file.close()
+    previous_path = path
+    try:
+        global new_path
+
+        if status == 'new':
+            root.dic_name = filedialog.askdirectory(title='Wybierz folder zapisu')
+            name = simpledialog.askstring('Nazwa pliku', 'Podaj nazwe pliku')
+            new_path = root.dic_name + '/' + name
+        elif status == 'existed':
+            root.file_name = filedialog.askopenfilename(title='Wybierz plik', filetype=[('db files', '*.db')])
+            root.file_name = root.file_name
+            new_path = root.file_name.replace('.db', '')
+
+        # Changing database's path in the path_file.txt
+        writing_file = open("paths/path_file.txt", "w")
+        if new_path == '':
+            writing_file.write(previous_path)
+        else:
+            writing_file.write(new_path)
+        writing_file.close()
+
+        file = open("paths/path_file.txt", "a+")
+        file.seek(0)
+        path = file.readline()
+        data = sqlite3.connect(f'{path}.db')
+        file.close()
+
+        # Create cursor
+        cursor = data.cursor()
+
+        # Create table
+        if status == 'new':
+            cursor.execute("""CREATE TABLE addresses (
+                                        systolic_arterial_pressure integer,
+                                        diastolic_blood_pressure integer,
+                                        formatted_date text) 
+                                    """)
+
+        # Commit Changes
+        data.commit()
+
+        # Close Connection
+        data.close()
+    except TypeError:
+        pass
+
+
 def options_screen():
-    pass
+    # Destroying redundant widgets
+    menu_label.destroy()
+    add_label.destroy()
+    database_button.destroy()
+    new_path_button.destroy()
+    quit_button.destroy()
+
+    # Defining new widgets
+    global brand_new_path, existed_path, options_label, back_from_options_button
+
+    options_label = Label(root, text='OPCJE', width=19, height=2, font="Verdana 32", bg='#161618', fg="white",
+                          relief="solid")
+    options_label.grid(row=0, column=0, padx=40, pady=15)
+    brand_new_path = Button(root, text='NOWY ZAPIS', width=20, height=4, command=lambda: new_path_window('new'))
+    brand_new_path.grid(row=1, column=0, pady=15)
+    existed_path = Button(root, text='OTWÓRZ ZAPIS', width=20, height=4, command=lambda: new_path_window('existed'))
+    existed_path.grid(row=2, column=0, pady=5)
+    back_from_options_button = Button(root, text='POWRÓT', width=20, height=4, command=lambda: back('options_screen'))
+    back_from_options_button.grid(row=3, column=0, pady=5)
     
 
 def menu_screen():
